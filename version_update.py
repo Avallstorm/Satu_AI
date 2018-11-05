da5c5d56f592c00aa6bbba93db8872b5b9515b8e
Avallstorm <stosava@gmail.com>
2018-11-05 15:33:26 -0500

    slight change to location of model

--- version_update.py
+++ version_update.py
@@ -1,8 +1,8 @@
 from AI_class import AI
 
-model 	= "../models/model_0_2/Satu_0_2_weights.hdf5"
-config  = "../models/model_0_2/Satu_0_2_config.json"
-vocab 	= "../models/model_0_2/Satu_0_2_vocab.json"
+model 	= "models/model_0_2/Satu_0_2_weights.hdf5"
+config  = "models/model_0_2/Satu_0_2_config.json"
+vocab 	= "models/model_0_2/Satu_0_2_vocab.json"
 keys  	= "keyfile"
 rejects = "rejects"
 prefs 	= "prefixes"
